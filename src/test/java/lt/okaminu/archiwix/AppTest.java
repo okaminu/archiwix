844bbba54e84de33868cb8cf0c6772154189f2c9
Okaminu <aurimasdgt@gmail.com>
2020-04-09 12:21:34 +0300

    Created sample project

---
+++ src/test/java/lt/okaminu/archiwix/AppTest.java
@@ -0,0 +1,14 @@
+package lt.okaminu.archiwix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AppTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
