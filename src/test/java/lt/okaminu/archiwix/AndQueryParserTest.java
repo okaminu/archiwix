ed8c34683503d6e8559cd240c31b4647bdfae44e
Okaminu <aurimasdgt@gmail.com>
2020-04-12 14:44:44 +0300

    Refactored test suite so that each query has it's own test class

---
+++ src/test/java/lt/okaminu/archiwix/AndQueryParserTest.java
@@ -0,0 +1,158 @@
+package lt.okaminu.archiwix;
+
+import lt.okaminu.archiwix.parser.InvalidQueryException;
+import lt.okaminu.archiwix.parser.QueryParser;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Set.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AndQueryParserTest {
+
+    private final QueryParser queryParser = new QueryParser();
+
+    @Test
+    public void parsesAND() {
+        Record greenRecord = new Record("greenId", "RandomTitle");
+        Record blueRecord = new Record("blueId", "RandomTitle");
+
+        String query = "AND(EQUAL(id,\"blueId\"),EQUAL(title,\"RandomTitle\"))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, blueRecord);
+
+        assertEquals(of(blueRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfNOT() {
+        Record redRecord = new Record("redId", "RandomTitle");
+        Record greenRecord = new Record("greenId", "RandomTitle");
+        Record blueRecord = new Record("blueId", "SomeTitle");
+
+        String query = "AND(EQUAL(title,\"RandomTitle\"),NOT(EQUAL(id,\"redId\")))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, blueRecord, redRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfDoubleNOT() {
+        Record redRecord = new Record("redId");
+        Record greenRecord = new Record("greenId");
+        Record blueRecord = new Record("blueId");
+
+        String query = "AND(NOT(EQUAL(id,\"blueId\")),NOT(EQUAL(id,\"redId\")))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, blueRecord, redRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfLESSWithGREATER() {
+        Record redRecord = new Record("redId", "", "", 2);
+        Record greenRecord = new Record("greenId", "", "", 5);
+        Record blueRecord = new Record("blueId", "", "", 10);
+
+        String query = "AND(LESS_THAN(views,8),GREATER_THAN(views,3))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, blueRecord, redRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfSingleAND() {
+        Record greenRecord = new Record("greenId", "RandomTitle", "BoringContent", 5);
+        Set<Record> records = of(
+                new Record("yellowId", "RandomTitle", "InterestingContent"),
+                new Record("redId", "RandomTitle", "BoringContent", 10),
+                new Record("blueId", "SomeTitle"),
+                greenRecord
+        );
+
+        String subExpression = "AND(EQUAL(title,\"RandomTitle\"),EQUAL(content,\"BoringContent\")";
+        String query = "AND(" + subExpression + "),LESS_THAN(views,8))";
+        Set<Record> actualRecords = filterRecords(query, records);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfDoubleAND() {
+        Record greenRecord = new Record("greenId", "RandomTitle", "BoringContent", 5);
+        Set<Record> records = of(
+                new Record("yellowId", "RandomTitle", "InterestingContent"),
+                new Record("whiteId", "RandomTitle", "BoringContent", 2),
+                new Record("redId", "RandomTitle", "BoringContent", 10),
+                new Record("blueId", "SomeTitle"),
+                greenRecord
+        );
+
+        String subExpression1 = "AND(EQUAL(title,\"RandomTitle\"),EQUAL(content,\"BoringContent\"))";
+        String subExpression2 = "AND(LESS_THAN(views,8),GREATER_THAN(views,3))";
+        String query = "AND(" + subExpression1 + "," + subExpression2 + ")";
+        Set<Record> actualRecords = filterRecords(query, records);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfOR() {
+        Record blueRecord = new Record("blueId", "", "", 1);
+        Record whiteRecord = new Record("whiteId", "", "", 10);
+        Set<Record> records = of(
+                new Record("yellowId", "", "", 2),
+                new Record("greenId", "", "", 5),
+                new Record("redId", "", "", 7),
+                blueRecord,
+                whiteRecord
+        );
+
+        String subExpression1 = "OR(EQUAL(id,\"blueId\"),EQUAL(id,\"whiteId\"))";
+        String subExpression2 = "OR(LESS_THAN(views,3),GREATER_THAN(views,6))";
+        String query = "AND(" + subExpression1 + "," + subExpression2 + ")";
+        Set<Record> actualRecords = filterRecords(query, records);
+
+        assertEquals(of(blueRecord, whiteRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesANDCompositeOfORWithNOT() {
+        Record blueRecord = new Record("blueId", "", "", 1);
+        Record whiteRecord = new Record("whiteId", "", "", 10);
+        Set<Record> records = of(
+                new Record("yellowId", "", "", 2),
+                new Record("greenId", "", "", 5),
+                new Record("redId", "", "", 7),
+                blueRecord,
+                whiteRecord
+        );
+
+        String subExpression1 = "OR(EQUAL(id,\"blueId\"),EQUAL(id,\"whiteId\"))";
+        String subExpression2 = "OR(NOT(LESS_THAN(views,6)),NOT(GREATER_THAN(views,3)))";
+        String query = "AND(" + subExpression1 + "," + subExpression2 + ")";
+        Set<Record> actualRecords = filterRecords(query, records);
+
+        assertEquals(of(blueRecord, whiteRecord), actualRecords);
+    }
+
+    @Test
+    public void throwsExceptionWhenQueryIsInvalid() {
+        assertThrowsInvalidQueryException("AND(views,123)");
+        assertThrowsInvalidQueryException("AND(EQUAL(id,\"green-id123\"))");
+    }
+
+    private void assertThrowsInvalidQueryException(String query, Record ...records) {
+        assertThrows(InvalidQueryException.class, () -> filterRecords(query, records));
+    }
+
+    private Set<Record> filterRecords(String query, Record ...records) {
+        return filterRecords(query, of(records));
+    }
+
+    private Set<Record> filterRecords(String query, Set<Record> records) {
+        return records.stream().filter(queryParser.parse(query)).collect(Collectors.toSet());
+    }
+}
---
+++ src/test/java/lt/okaminu/archiwix/NotQueryParserTest.java
@@ -0,0 +1,99 @@
+package lt.okaminu.archiwix;
+
+import lt.okaminu.archiwix.parser.InvalidQueryException;
+import lt.okaminu.archiwix.parser.QueryParser;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Set.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NotQueryParserTest {
+
+    private final QueryParser queryParser = new QueryParser();
+
+    @Test
+    public void parsesNOT() {
+        Record greenRecord = new Record("green-id123");
+        Record blueRecord = new Record("blue-id123");
+
+        Set<Record> actualRecords = filterRecords("NOT(EQUAL(id,\"green-id123\"))", greenRecord, blueRecord);
+
+        assertEquals(of(blueRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfNOT() {
+        Record greenRecord = new Record("green-id123");
+        Record blueRecord = new Record("blue-id123");
+
+        Set<Record> actualRecords = filterRecords("NOT(NOT(EQUAL(id,\"green-id123\")))", greenRecord, blueRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfMultipleNOTs() {
+        Record greenRecord = new Record("green-id123");
+        Record blueRecord = new Record("blue-id123");
+
+        Set<Record> actualRecords = filterRecords("NOT(NOT(NOT(EQUAL(id,\"green-id123\"))))", greenRecord, blueRecord);
+
+        assertEquals(of(blueRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfLESS() {
+        Record greenRecord = new Record("greenId", "", "", 10);
+        Record blueRecord = new Record("blueId", "", "", 5);
+
+        Set<Record> actualRecords = filterRecords("NOT(LESS_THAN(views,6))", greenRecord, blueRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfGREATER() {
+        Record greenRecord = new Record("greenId", "", "", 10);
+        Record blueRecord = new Record("blueId", "", "", 5);
+
+        Set<Record> actualRecords = filterRecords("NOT(GREATER_THAN(views,6))", greenRecord, blueRecord);
+
+        assertEquals(of(blueRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfAND() {
+        Record redRecord = new Record("redId", "RandomTitle");
+        Record greenRecord = new Record("greenId", "SomeTitle");
+
+        String query = "NOT(AND(EQUAL(title,\"RandomTitle\"),EQUAL(id,\"redId\")))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, redRecord);
+
+        assertEquals(of(greenRecord), actualRecords);
+    }
+
+    @Test
+    public void parsesNOTOfOR() {
+        Record redRecord = new Record("redId");
+        Record greenRecord = new Record("greenId");
+        Record blueRecord = new Record("blueId");
+
+        String query = "NOT(OR(EQUAL(id,\"redId\"),EQUAL(id,\"greenId\")))";
+        Set<Record> actualRecords = filterRecords(query, greenRecord, blueRecord, redRecord);
+
+        assertEquals(of(blueRecord), actualRecords);
+    }
+
+    @Test
+    public void throwsExceptionWhenQueryIsInvalid() {
+        assertThrows(InvalidQueryException.class, () -> filterRecords("NOT(views,123)"));
+    }
+
+    private Set<Record> filterRecords(String query, Record ...records) {
+        return of(records).stream().filter(queryParser.parse(query)).collect(Collectors.toSet());
+    }
+}
